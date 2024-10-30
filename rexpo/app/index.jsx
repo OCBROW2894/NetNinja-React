ec8409fe41e80f599d75e6235f17c7b8d9cf687b
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2024-10-31 00:43:04 +0300

    Nothing Important

--- rexpo/app/index.jsx
+++ rexpo/app/index.jsx
@@ -7,7 +7,7 @@ export default function App() {
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-3xl">Aora !</Text>
       <StatusBar style="auto" />
-      <Link href="/profile" className="mt-4 text-blue-600 underline">Go to Profile</Link>
+      <Link href="/profile" className="mt-4 text-blue-600 underline">Go To Profile</Link>
     </View>
   );
 }
