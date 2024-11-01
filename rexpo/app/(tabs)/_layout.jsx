8e1fd994ed94fb7105c41526392b8d30da960bca
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2024-11-01 16:38:25 +0300

    Added the home icon and Started customising

--- rexpo/app/(tabs)/_layout.jsx
+++ rexpo/app/(tabs)/_layout.jsx
@@ -1,11 +1,43 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
+import { Tabs, Redirect } from 'expo-router'
 import React from 'react'
+import { icons } from '../../constants';
+
+const TabIcon = ({color, focused, icon, name}) => {
+    return (
+        <View>
+            <Image
+             source={icon}
+             resizeMode= "contain"
+             tintColor={color}
+             className="w-6 h-6"
+              />
+              <Text className={`${focused ? 'font-psemibold' : 'font-pregular'} text-xs`}>{name}</Text>
+        </View>
+    )
+}
 
 const TabsLayout = () => {
   return (
-    <View>
-      <Text>TabsLayout</Text>
-    </View>
+    <>
+      <Tabs>
+        <Tabs.Screen
+         name="home" 
+         options={{
+          headerShown: false,
+          title: 'Home',
+          tabBarIcon: ({color, focused}) => (
+           <TabIcon
+           color={color}
+           focused={focused}
+           icon={icons.home}
+           name="Home"
+           />
+          ),
+         }}
+         />
+      </Tabs>
+    </>
   )
 }
 
--- rexpo/app/index.jsx
+++ rexpo/app/index.jsx
@@ -7,7 +7,7 @@ export default function App() {
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-3xl font-pblack">Aora !</Text>
       <StatusBar style="auto" />
-      <Link href="/profile" className="mt-4 text-blue-600 underline">Go To Profile</Link>
+      <Link href="/home" className="mt-4 text-blue-600 underline">Go To Home</Link>
     </View>
   );
 }
