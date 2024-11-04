57b52496fba9beb76547c5a1f4dcce1ba13e948a
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2024-11-04 15:46:35 +0300

    Fixed the Babel Error and Added more icons and Name to the Tab section

--- rexpo/app/(tabs)/_layout.jsx
+++ rexpo/app/(tabs)/_layout.jsx
@@ -5,14 +5,14 @@ import { icons } from '../../constants';
 
 const TabIcon = ({color, focused, icon, name}) => {
     return (
-        <View className="items-center justify-center py-2">
+        <View className="items-center justify-center gap-2">
             <Image
              source={icon}
              resizeMode="contain"
              tintColor={color}
-             className="w-3 h-3"
+             className="w-5 h-5"
               />
-              <Text className={`${focused ? 'font-psemibold' : 'font-pregular'} text-xs mt-1`}>{name}</Text>
+              <Text className={`${focused ? 'font-psemibold' : 'font-pregular'} text-xs`}>{name}</Text>
         </View>
     )
 }
@@ -22,12 +22,7 @@ const TabsLayout = () => {
     <>
       <Tabs
       screenOptions={{
-        tabBarShowLabel: false,
-        tabBarStyle: {
-          height: 65,
-          paddingBottom: 10,
-          paddingTop: 5,
-        },
+        tabBarShowLabel: false
       }}
       >
         <Tabs.Screen
@@ -45,6 +40,54 @@ const TabsLayout = () => {
           ),
          }}
          />
+
+          <Tabs.Screen
+              name="bookmark"
+              options={{
+                  headerShown: false,
+                  title: 'Bookmark',
+                  tabBarIcon: ({color, focused}) => (
+                      <TabIcon
+                          color={color}
+                          focused={focused}
+                          icon={icons.bookmark}
+                          name="Bookmark"
+                      />
+                  ),
+              }}
+          />
+
+          <Tabs.Screen
+              name="create"
+              options={{
+                  headerShown: false,
+                  title: 'Create',
+                  tabBarIcon: ({color, focused}) => (
+                      <TabIcon
+                          color={color}
+                          focused={focused}
+                          icon={icons.plus}
+                          name="Create"
+                      />
+                  ),
+              }}
+          />
+
+          <Tabs.Screen
+              name="profile"
+              options={{
+                  headerShown: false,
+                  title: 'Profile',
+                  tabBarIcon: ({color, focused}) => (
+                      <TabIcon
+                          color={color}
+                          focused={focused}
+                          icon={icons.profile}
+                          name="Profile"
+                      />
+                  ),
+              }}
+          />
       </Tabs>
     </>
   )
