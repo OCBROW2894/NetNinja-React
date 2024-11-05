6b620e0825adb45bf2bf157df9c2666643c2ae21
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2024-11-05 18:30:06 +0300

    Worked the distucturising the custom button props

--- rexpo/app/components/customButton.jsx
+++ rexpo/app/components/customButton.jsx
@@ -1,7 +1,7 @@
 import { TouchableOpacity, Text } from 'react-native'
 import React from 'react'
 
-const CustomButton = () => {
+const CustomButton = ({ title, handlePress, containerStyles, textStyles, isLoading}) => {
   return (
     <TouchableOpacity className={'bg-secondary rounded-xl min-h-[62px] items-center justify-center'}>
       <Text className="text-primary font-psemibold text-lg">CustomButton</Text>
