c8101f29377971b08969d7419fe279f2f6e37dd1
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2024-11-07 15:07:35 +0300

    Fixed the Border color change problem

--- rexpo/app/components/FormField.jsx
+++ rexpo/app/components/FormField.jsx
@@ -1,5 +1,5 @@
 import { View, Text, TextInput, Image, TouchableOpacity } from 'react-native'
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { icons} from '../../constants';
 
 const FormField = ({
@@ -10,30 +10,34 @@ const FormField = ({
     handleChangeText, 
     ...props}) => {
     const [showPassword, setShowPassword] = useState(false);
+    const [isFocused, setIsFocused] = useState(false);
+
   return (
     <View className={`space-y-2 ${otherStyles}`}>
       <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
 
-      <View className="w-full h-16 px-4 bg-black-100 rounded-2xl  border-black-200 focus:border-secondary flex flex-row items-center">
+      <View className={`w-full h-16 px-4 bg-black-100 rounded-2xl border ${isFocused ? 'border-secondary' : 'border-black-200'} flex flex-row items-center`}>
         <TextInput
-        className="flex-1 text-white font-psemibold text-base"
-        placeholder={placeholder}
-        placeholderTextColor="#7B7B8B"
-        value={value}
-        onChangeText={handleChangeText}
-        secureTextEntry={title === 'Password' && !showPassword}
-        {...props}
+          className="flex-1 text-white font-psemibold text-base"
+          placeholder={placeholder}
+          placeholderTextColor="#7B7B8B"
+          value={value}
+          onChangeText={handleChangeText}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+          secureTextEntry={title === 'Password' && !showPassword}
+          {...props}
         />
 
         {title === "Password" && (
             <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
                 <Image 
-                source= {!showPassword ? icons.eye : icons.eyeHide} 
-                classname="w-6 h-6"
+                source={!showPassword ? icons.eye : icons.eyeHide} 
+                className="w-6 h-6"
                 resizeMode="contain" 
                 />
             </TouchableOpacity>
-            )}
+        )}
       </View>
     </View>
   )
