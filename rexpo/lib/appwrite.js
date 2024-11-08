4412fc00c08d38177889b70e18a9f45fd61001ce
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2024-11-08 15:10:08 +0300

    worked on creating a new user

--- rexpo/lib/appwrite.js
+++ rexpo/lib/appwrite.js
@@ -1,4 +1,6 @@
-import { Client } from 'react-native-appwrite';
+import { Account, Client, ID } from 'react-native-appwrite';
+import { Avatars } from 'react-native-appwrite';
+
 export const config = {
     endpoint: 'https://cloud.appwrite.io/v1',
     platform: 'com.ocbrow.aora',
@@ -12,9 +14,32 @@ export const config = {
 
 // Init your React Native SDK
 const client = new Client();
+const avatars = new Avatars(client);
 
 client
     .setEndpoint(config.endpoint) // Your Appwrite Endpoint
     .setProject(config.projectId) // Your project ID
     .setPlatform(config.platform) // Your application ID or bundle ID.
-;
+
+
+const account = new Account(client);
+
+
+export const createUser = async (email, password, username) => {
+    try {
+       const newAccount = await account.create(
+        ID.unique(),
+        email,
+        password,
+        username
+       )
+
+       if(!newAccount) throw Error;
+
+       const avatarUrl = avatars.getInitials(username);
+    } catch (error) {
+        console.log(error);
+        throw new Error(error);
+    }
+}
+
