1e280321c408cc15d65465a4cc220976f89fa745
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2024-11-08 14:20:33 +0300

    Initialized React Native SDK

--- rexpo/lib/appwrite.js
+++ rexpo/lib/appwrite.js
@@ -1,4 +1,5 @@
-export const appwriteConfig = {
+import { Client } from 'react-native-appwrite';
+export const config = {
     endpoint: 'https://cloud.appwrite.io/v1',
     platform: 'com.ocbrow.aora',
     projectId: '672de793001a97a3092a',
@@ -7,3 +8,13 @@ export const appwriteConfig = {
     videoCollectionId: '672deba2002482045187',
     storageId: '672defa40019caf73054'
 }
+
+
+// Init your React Native SDK
+const client = new Client();
+
+client
+    .setEndpoint(config.endpoint) // Your Appwrite Endpoint
+    .setProject(config.projectId) // Your project ID
+    .setPlatform(config.platform) // Your application ID or bundle ID.
+;
