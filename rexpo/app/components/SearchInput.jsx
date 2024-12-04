b289b5723f9b7c5e8419743c41516baad84a3af7
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2024-12-04 17:30:15 +0300

    created a new component and added itvto the home screen

---
+++ rexpo/app/components/SearchInput.jsx
@@ -0,0 +1,45 @@
+import { View, Text, TextInput, Image, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { icons} from '../../constants';
+
+const SearchInput = ({
+    title,
+    value, 
+    otherStyles, 
+    placeholder, 
+    handleChangeText, 
+    ...props}) => {
+    const [showPassword, setShowPassword] = useState(false);
+    const [isFocused, setIsFocused] = useState(false);
+  return (
+    <View className={`space-y-2 ${otherStyles}`}>
+      <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
+
+      <View className={`w-full h-16 px-4 bg-black-100 rounded-2xl border ${isFocused ? 'border-secondary' : 'border-black-200'} flex flex-row items-center`}>
+        <TextInput
+        className="flex-1 text-white font-psemibold text-base"
+        placeholder={placeholder}
+        placeholderTextColor="#7B7B8B"
+        value={value}
+        onChangeText={handleChangeText}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        secureTextEntry={title === 'Password' && !showPassword}
+        {...props}
+        />
+
+        {title === "Password" && (
+            <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+                <Image 
+                source= {!showPassword ? icons.eye : icons.eyeHide} 
+                className="w-6 h-6"
+                resizeMode="contain" 
+                />
+            </TouchableOpacity>
+            )}
+      </View>
+    </View>
+  )
+}
+
+export default SearchInput
