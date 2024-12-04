0a217ae6dd8f53c31fd62249227f4df63ba8feb5
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2024-12-04 16:30:40 +0300

    started working on the Home Screen

--- rexpo/app/(tabs)/home.jsx
+++ rexpo/app/(tabs)/home.jsx
@@ -1,11 +1,35 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList} from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView>
+        <FlatList
+            data={[{ id : 1}, { id : 2}, { id : 3}]}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => (
+              <Text className="text-primary text-3xl font-bold">
+                {item.id}
+              </Text>
+            )}
+
+            ListHeaderComponent={()=> (
+              <View className="my-6 px-4 space-y-6">
+                <View className="flex-row justify-between items-start mb-6">
+                  <View>
+                    <Text>
+                      Welcome Back
+                    </Text>
+                    <Text>
+                      OCBROW_2894
+                    </Text>
+                  </View>
+                </View>
+              </View>
+            )}
+        />
+    </SafeAreaView>
   )
 }
 
