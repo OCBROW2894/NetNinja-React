bbec773b7f509c4bce02c6a8d1b15dd92c6a33c5
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2024-12-06 15:07:30 +0300

    customised the Empty State Component

--- rexpo/app/(tabs)/home.jsx
+++ rexpo/app/(tabs)/home.jsx
@@ -4,12 +4,15 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../constants'
 import SearchInput from '../components/SearchInput'
 import Trending from '../components/Trending'
+import EmptyState from '../components/EmptyState'
+
 
 const Home = () => {
   return (
     <SafeAreaView className="bg-primary">
         <FlatList
-            data={[{ id : 1}, { id : 2}, { id : 3}]}
+            //data={[{ id : 1}, { id : 2}, { id : 3}]}
+            data={[]}
             keyExtractor={item => item.id}
             renderItem={({ item }) => (
               <Text className="text-primary text-3xl font-bold text-white">
@@ -49,7 +52,10 @@ const Home = () => {
               </View>
             )}
             ListEmptyComponent={()=>(
-              <EmptyState/>
+              <EmptyState
+              title="No Videos Found"
+              subtitle="Be The Firstone to upload a video"
+              />
             )}
         />
     </SafeAreaView>
--- rexpo/app/components/EmptyState.jsx
+++ rexpo/app/components/EmptyState.jsx
@@ -1,10 +1,21 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import { images } from '../../constants'
 
-const EmptyState = () => {
+const EmptyState = ({ title, subtitle }) => {
   return (
-    <View>
-      <Text>EmptyState</Text>
+    <View className="justify-center items-center px-4">
+        <Image
+        source={images.empty}
+        className="w-[270px] h-[215px]"
+        resizeMode='contain'
+         />
+      <Text className="text-xl text-center font-psemibold text-white mt-2">
+        {title}
+      </Text>
+      <Text className="font-pmedium text-sm text-gray-100">
+       {subtitle}
+       </Text>
     </View>
   )
 }
