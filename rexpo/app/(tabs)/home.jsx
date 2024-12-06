e4cd6ee9392a8e55fbe82f5ba06e136ed504a0dd
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2024-12-06 14:22:33 +0300

    Created the New component for the trending sector

--- rexpo/app/(tabs)/home.jsx
+++ rexpo/app/(tabs)/home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../constants'
 import SearchInput from '../components/SearchInput'
-
+import Trending from '../components/Trending'
 const Home = () => {
   return (
     <SafeAreaView className="bg-primary">
@@ -38,8 +38,12 @@ const Home = () => {
                 <SearchInput/>
                 <View className="w-full flex-1 pt-5 pb-8">
                   <Text className="text-gray-100 text-lg font-pregular mb-3">
-                    Latest Videos
+                    Trending Videos
                   </Text>
+
+                  <Trending
+                  posts={[{id: 1}, {id: 2}, {id: 3}] ?? []}
+                  />
                 </View>
               </View>
             )}
