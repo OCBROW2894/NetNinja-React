e4babd2c2f55cff8a683011bbad323854b8efcca
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2024-12-11 13:41:59 +0300

    Created the videocard component

--- rexpo/app/(tabs)/home.jsx
+++ rexpo/app/(tabs)/home.jsx
@@ -7,6 +7,7 @@ import Trending from '../components/Trending'
 import EmptyState from '../components/EmptyState'
 import { getAllPosts } from '../../lib/appwrite'
 import useAppwrite from '../../lib/useAppwrite'
+import VideoCard from '../components/VideoCard'
 
 
 const Home = () => {
@@ -28,9 +29,7 @@ const Home = () => {
            data={posts}
             keyExtractor={item => item.id}
             renderItem={({ item }) => (
-              <Text className="text-primary text-3xl font-bold text-white">
-                {item.title}
-              </Text>
+              <VideoCard />
             )}
 
             ListHeaderComponent={()=> (
