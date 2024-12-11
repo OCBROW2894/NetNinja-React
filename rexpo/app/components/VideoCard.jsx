953351223931955762fcc5c0bcfb1ccdf2fd9d62
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2024-12-11 14:07:26 +0300

    Added images to the video card

--- rexpo/app/components/VideoCard.jsx
+++ rexpo/app/components/VideoCard.jsx
@@ -1,9 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 
 const VideoCard = ({ video: {title, thumbnail, video, creator: {username, avatar}} }) => {
   return (
     <View className="flex-col items-center px-4 mb-14">
+        <View className="flex-row gap-3 items-start">
+            <View className="justify-center items-center flex-row flex-1">
+                <View className="w-[46px] h-[46px] rounded-lg border border-seondary justify-center items-center p-0.5">
+                    <Image
+                    source={{uri: avatar}}
+                    className="w-full h-full rounded-lg"
+                    resizeMode= "cover" />
+                </View>
+            </View>
+        </View>
       <Text className="text-2xl text-white">{title}</Text>
     </View>
   )
