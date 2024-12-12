9de3ad248ee7624abc41f5b4670e858ff20aa5b5
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2024-12-12 17:05:44 +0300

    started working on the animation

--- rexpo/app/components/Trending.jsx
+++ rexpo/app/components/Trending.jsx
@@ -1,10 +1,34 @@
 import { View, Text, FlatList } from 'react-native'
 import React from 'react'
+import { useState } from 'react'
 import * as Animatable from 'react-native-animatable';
 
-const TrendingItem =() =>{
+const zoomIn = {
+  0: {
+    scale:0.9
+},
+1: {
+    scale:1,
+  }
+}
+
+const zoomOut = {
+  0: {
+    scale:1
+},
+1: {
+    scale:0.9,
+  }
+}
+
+const TrendingItem =({activeItem, item}) =>{
+  const [play, setPlay] = useState(false);
   return(
-    <Animatable.View>
+    <Animatable.View
+    className="mr-5"
+    animation={activeItem === item.$id ? zoomIn : zoomOut}
+    duration={500}
+    >
 
     </Animatable.View>
   )
