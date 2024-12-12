efc69a893e19c095163a59120b54c5574f4e56c9
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2024-12-12 16:53:44 +0300

    created the active usestate

--- rexpo/app/components/Trending.jsx
+++ rexpo/app/components/Trending.jsx
@@ -2,21 +2,22 @@ import { View, Text, FlatList } from 'react-native'
 import React from 'react'
 import * as Animatable from 'react-native-animatable';
 
-const trendingItem =() =>{
+const TrendingItem =() =>{
   return(
     <Animatable.View>
-      
+
     </Animatable.View>
   )
 }
 
 const Trending = ({posts}) => {
+  const [activeItem, setactiveItem] = useState(posts[0]);
   return (
     <FlatList
     data={posts}
     keyExtractor={(item) => item.$id}
     renderItem={({item}) => (
-        <Text className="text-3xl text-white">{item.id}</Text>
+        <TrendingItem activeItem={activeItem} item={item} />
     )}
     horizontal
     />
