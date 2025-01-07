bbdf124d90639224ac6d6a0ff453278dbaaa3a01
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-01-07 14:07:15 +0300

    Fixed the video card styling issues

--- rexpo/app/components/VideoCard.jsx
+++ rexpo/app/components/VideoCard.jsx
@@ -10,14 +10,14 @@ const VideoCard = ({ video: {title, thumbnail, video, creator: {username, avatar
     <View className="flex-col items-center px-4 mb-14">
         <View className="flex-row gap-3 items-start">
             <View className="justify-center items-center flex-row flex-1">
-                <View className="w-[46px] h-[46px] rounded-lg border border-seondary justify-center items-center p-0.5">
+                <View className="w-[46px] h-[46px] rounded-lg border border-secondary flex justify-center items-center p-0.5">
                     <Image
                     source={{uri: avatar}}
                     className="w-full h-full rounded-lg"
                     resizeMode= "cover" />
                 </View>
 
-                <View className="justify-center flex-1 ml-3 gap-y-1">
+                <View className=" flex justify-center flex-1 ml-3 gap-y-1">
                     <Text className="text-white font-psemibold text-sm" numberOfLines={1}>{title}</Text>
                     <Text className="text-xs text-gray-100 font-pregular" numberOfLines={1}>{username}</Text>
                 </View>
@@ -33,24 +33,24 @@ const VideoCard = ({ video: {title, thumbnail, video, creator: {username, avatar
         </View>
 
         {play ? (
-        <Video
-        source={{uri: video}}
-        className="w-full h-60 rounded-xl mt-3 "
-        resizeMode={ResizeMode.CONTAIN}
-        useNativeControls
-        shouldPlay
-        // Reset play state when video finishes
-        onPlaybackStatusUpdate={(status)=> {
-          if(status.didJustFinish) {
-            setPlay(false);
-          }
-        }}
-        />
+            <Video
+                source={{uri: video}}
+                className="w-full h-60 rounded-xl mt-3 "
+                resizeMode={ResizeMode.CONTAIN}
+                useNativeControls
+                shouldPlay
+                // Reset play state when video finishes
+                onPlaybackStatusUpdate={(status)=> {
+                    if(status.didJustFinish) {
+                        setPlay(false);
+                    }
+                }}
+            />
         ) :(
             <TouchableOpacity
             activeOpacity={0.7}
             onPress={() => setPlay(true)}
-            className="w-full h-full rounded-xl mt-3 relative justify-center items-center">
+            className="w-full h-60 rounded-xl mt-3 relative flex justify-center items-center">
                 <Image
                 source= {{uri: thumbnail}}
                 className="w-full h-full rounded-xl mt-3"
@@ -67,4 +67,4 @@ const VideoCard = ({ video: {title, thumbnail, video, creator: {username, avatar
   )
 }
 
-export default VideoCard
+export default VideoCard;
